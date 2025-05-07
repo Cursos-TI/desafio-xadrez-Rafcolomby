c91021eec89ba57270e57ee9e30eabe58c86e26c
Rafcolomby <rafcolomby@hotmail.com>
2025-05-06 21:45:34 -0300

    xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -3,7 +3,6 @@
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-#include <stdio.h>
 
 int main() {
     int i; // Variável de controle para loops
